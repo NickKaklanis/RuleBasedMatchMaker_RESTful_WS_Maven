5502ff0466e50c231d16bf78010cbc9bc5540b86
Cloud4AllTUD <claudia.loitsch@tu-dresden.de>
2015-07-10 12:31:25 +0200

    Add NATC, NATC_ RES and Test case MMTest4a
    adds new conflict NATC - NoATConflict - that detects if a user preference,
    which are indicated as required, but no configuration could be inferred.
    
    substitutePreference
    * create a knowledge sete to specify substitutaion and recommendation for
    * prefernces that cannot be matched due to missing solution support, e.g
    * magnification preference but no Magnifier available.
    
    config
    * include KS substitutePrefernece.jsonld in the reasoning
    
    semantic/sematicsSolutionMMEval
    * add screen resolution
    
    rules/conflictDetection
    * add rule to detect NATC
    
    rules/conflictResolution
    * add rule to resolve NATC
    
    MMTest4aOut; MMTest4a; RBMM_WebServiceIT
    * add test case MMTest4a

--- src/test/java/com/gpii/restful/RBMM_WebServiceIT.java
+++ src/test/java/com/gpii/restful/RBMM_WebServiceIT.java
@@ -23,6 +23,7 @@ public class RBMM_WebServiceIT extends TestCase
 			_MMTest1d();
 			_MMTest2();
 			_MMTest3();			
+			_MMTest4a();
 			//_resolveMSC_MultiSolutionPreffered();
 			//_resolveMSC_OneSolutionPreffered();
 			
@@ -147,7 +148,19 @@ public class RBMM_WebServiceIT extends TestCase
         String filepathExpectedOUT = System.getProperty("user.dir") + "/src/main/webapp/WEB-INF/testData/expectedTestOutcomes/MMTest3OUT.json";
 	    
 	    performTest(filepathIN, filepathExpectedOUT, "_MMTest3");           
-    }     
+    }
+    public void _MMTest4a()
+    {
+    	System.out.println("\n******************_MMTest4a ***************************");
+        System.out.println("** No AT of the type required by the user is installed.*");
+        System.out.println("** No Magnifier on Windows. ****************************");
+        System.out.println("\n******************************************************");    	
+	
+        String filepathIN = System.getProperty("user.dir") + "/src/main/webapp/WEB-INF/testData/preferences/MMTest4a.json";
+        String filepathExpectedOUT = System.getProperty("user.dir") + "/src/main/webapp/WEB-INF/testData/expectedTestOutcomes/MMTest4aOUT.json";
+	    
+	    performTest(filepathIN, filepathExpectedOUT, "_MMTest4a");           
+    }      
     private void _resolveMSC_MultiSolutionPreffered()
     {
     	System.out.println("\n*************************************************************************");
